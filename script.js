9d9ccca305e42abb30100848b2bcca270a77b11b
RoyOtten <99065498@mydavinci.nl>
2021-12-14 15:37:38 +0100

    tweede commit

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 var btn = document.createElement('button');
-btn.style.margin = '10px';
-btn.innerHTML = '...';
+btn.style.margin = '20px';
+btn.innerHTML = 'CLICK ME';
 document.body.appendChild(btn);
 
+
 // schijf hier tussen je code
+
+
+var button = true;
+function myfunction(){
+    if (button === true){
+        document.body.style.backgroundColor = "yellow";
+        button = false;
+    }
+    else if (button === false) {
+        document.body.style.backgroundColor = "black";
+        button = true;
+    }
+}
+btn.addEventListener('click', () => {
+    myfunction()
+})
+
 
 
 
